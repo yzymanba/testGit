d5a8cd4243da4600bd8b1af548bb040189e8aa65
yzy <729158389@qq.com>
2021-04-01 15:05:24 +0800

    主干添加

--- src/main/java/com/yzy/git/Hello.java
+++ src/main/java/com/yzy/git/Hello.java
@@ -10,6 +10,7 @@ public class Hello {
         System.out.println("hello git");
         System.out.println("更新1");
         System.out.println("创建分支");
+        System.out.println("主干添加");
 
     }
 }
