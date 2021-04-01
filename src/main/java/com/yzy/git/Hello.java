4c4a6a12308e23cce919f27eccd87bc32bc7ef28
yzy <729158389@qq.com>
2021-04-01 14:57:23 +0800

    文件添加1

---
+++ src/main/java/com/yzy/git/Hello.java
@@ -0,0 +1,13 @@
+package com.yzy.git;
+
+/**
+ * @author yzy
+ * @version 1.8 1.11
+ * @create 2021-04-01  14:42
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
